72ef8143f647509e35a5d1a72186ebd4bc41754e
Christian Fobel <christian@fobel.net>
2011-03-29 16:50:58 -0400

    Add checked versions of upper/lower_bound...
    Add checked versions of upper/lower_bound to ensure results are valid.

--- upper_lower_bound.cpp
+++ upper_lower_bound.cpp
@@ -5,18 +5,56 @@
 #include <vector>
 using namespace std;
 
+vector<int>::iterator checked_upper_bound(vector<int>::iterator begin,
+        vector<int>::iterator end, int value) {
+    vector<int>::iterator up = upper_bound(begin, end, value);
+    if(up == end) {
+        cout << value << " is greater than last value: " << *(end - 1) << endl;
+    } else {
+        cout << "upper_bound at position " << int(up - begin) << endl;
+    }
+    return up;
+}
+
+
+vector<int>::iterator checked_lower_bound(vector<int>::iterator begin,
+        vector<int>::iterator end, int value) {
+    vector<int>::iterator low = lower_bound(begin, end, value);
+    if(low == end) {
+        cout << value << " is greater than last value: " << *(end - 1) << endl;
+    } else if(*low != value) {            
+        cout << value << " is less than first value: " << *begin << endl;
+    } else {
+        cout << "lower_bound at position " << int(low - begin) << endl;
+    }
+    return low;
+}
+
+
 int main () {
-  int myints[] = {10,20,30,30,20,10,10,20};
-  vector<int> v(myints, myints + 8);        // 10 20 30 30 20 10 10 20
-  vector<int>::iterator low, up;
+    int myints[] = {10, 20, 30, 30, 20, 10, 10, 20};
+    vector<int> v(myints, myints + 8);          // 10 20 30 30 20 10 10 20
+    vector<int>::iterator low, up;
 
-  sort(v.begin(), v.end());                 // 10 10 10 20 20 20 30 30
+    sort(v.begin(), v.end());                   // 10 10 10 20 20 20 30 30
 
-  low=lower_bound (v.begin(), v.end(), 20); //          ^
-  up= upper_bound (v.begin(), v.end(), 20); //                   ^
+    low = lower_bound(v.begin(), v.end(), 20);  //          ^
+    up = upper_bound(v.begin(), v.end(), 20);   //                   ^
 
-  cout << "lower_bound at position " << int(low - v.begin()) << endl;
-  cout << "upper_bound at position " << int(up - v.begin()) << endl;
+    cout << "lower_bound at position " << int(low - v.begin()) << endl;
+    cout << "upper_bound at position " << int(up - v.begin()) << endl;
 
-  return 0;
+    cout << endl;
+
+    checked_lower_bound(v.begin(), v.end(), 5);
+    checked_lower_bound(v.begin(), v.end(), 20);
+    checked_lower_bound(v.begin(), v.end(), 40);
+
+    cout << endl;
+
+    checked_upper_bound(v.begin(), v.end(), 5);
+    checked_upper_bound(v.begin(), v.end(), 20);
+    checked_upper_bound(v.begin(), v.end(), 40);
+
+    return 0;
 }
